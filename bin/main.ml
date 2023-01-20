6218283682b81ee78d1a3a8956893c7a2ce0f40c
samyhaff <haffoudhisamy@gmail.com>
2023-01-20 15:21:19 +0100

    created graphics window

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+open Graphics
+
+let () =
+    open_graph "";
+    set_window_title "Turtle";
+    let x = size_x () / 2 in
+    let y = size_y () / 2 in
+    draw_circle x y 100;
+    ignore (wait_next_event [Key_pressed])
