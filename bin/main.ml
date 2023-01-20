753d7c1a78a36f1c5e691bda2820486ea4936ece
samyhaff <haffoudhisamy@gmail.com>
2023-01-20 15:57:03 +0100

    started working on execution

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,24 @@
 open Graphics
+open Turtle
 
-let () =
+let init () =
     open_graph "";
     set_window_title "Turtle";
     let x = size_x () / 2 in
     let y = size_y () / 2 in
-    draw_circle x y 100;
+    moveto x y
+
+let get_command () =
+    if Array.length Sys.argv = 1 then failwith "No command given"
+    else Driver.parse Sys.argv.(1)
+
+let exec command =
+    let (x, y)= current_point () in
+    let (new_x, new_y) = Driver.move command x y in
+    lineto new_x new_y
+
+let () =
+    init ();
+    let command = get_command () in
+    exec command;
     ignore (wait_next_event [Key_pressed])
--- lib/driver.ml
+++ lib/driver.ml
@@ -1,4 +1,16 @@
+open Ast
+
 let parse s =
     let lexbuf = Lexing.from_string s in
     let ast = Parser.prog Lexer.read lexbuf in
     ast
+
+let move command x y =
+    match command with
+    | Move (dir, n) ->
+            (match dir with
+            | Forward -> (x, y + n)
+            | Back -> (x, y - n)
+            | Right -> (x + n, y)
+            | Left -> (x - n, y))
+    | _ -> (x, y)
